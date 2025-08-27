7cc65d8b9fa3164a128c93c3954bcdb68507d9e9
Charlindo <charlesnoe30@gmail>
2025-08-27 20:08:44 -0300

    salvando

--- src/ui/Menu.kt
+++ src/ui/Menu.kt
@@ -6,5 +6,17 @@ fun menu() {
     println("3 - listar Caixa D'Água")
     println("4 - Excluir Caixa D'Água")
 
+    val opcao = readln().toInt()
+
+    when(opcao) {
+        1 -> println("Cadastrando caixa...")
+        2 -> println("Editando caixa...")
+        3 -> println("listar caixa...")
+        4 -> println("Excluido caixa...")
+        else -> println("Opção inválida")
+
+
+    }
 
 }
+
