b36e7097526af0ccdb159f1b49034c12e139b848
Charlindo <joao280304@edu.unipar.br>
2025-08-27 20:27:08 -0300

    aula27/08/25

--- src/crud/gerenciarCaixas.kt
+++ src/crud/gerenciarCaixas.kt
@@ -1,7 +1,33 @@
 package crud
 
+import enumeradores.Material
+
 fun cadastrarCaixa() {
-    
+//    val material: Material,
+//    val capidade: Int,
+//    val cor: String,
+//    val peso: Double,
+//    val preco: Double,
+//    val altura: Double,
+//    val profundidade: Double,
+//    val largura: Double,
+//    val marca: String,
+    println("Escolha o material do qual caixa é composta")
+    println("1 - Pvc") //PVC, PLÁSTICO, METAL, FITA
+    println("2 - Plástico")
+    println("3- Metal")
+    println("4 - Argamassa")
+    val opcao = readln().toInt()
+    var material : Material
+    when(opcao) {
+        1-> material = Material.PVC
+        2-> material = Material.PLÁSTICO
+        3-> material = Material.METAL
+        4-> material = Material.FITA
+        else -> material = Material.PLÁSTICO
+
+    }
+    println("Capacidade da Caixa: ")
 }
 
 fun editarCaixa() {
