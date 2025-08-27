4029847c05add8c0511df64ee531c3628bfa4ac7
Charlindo <charlesnoe30@gmail>
2025-08-27 20:15:11 -0300

    Salvando commits

---
+++ src/crud/gerenciarCaixas.kt
@@ -0,0 +1,17 @@
+package crud
+
+fun cadastrarCaixa() {
+    
+}
+
+fun editarCaixa() {
+
+}
+
+fun listarCaixa() {
+
+}
+
+fun excluirCaixa() {
+
+}
--- src/ui/Menu.kt
+++ src/ui/Menu.kt
@@ -1,22 +1,22 @@
 package ui
 
 fun menu() {
-    println("1 - Cadastrar Caixa D'Água")
-    println("2 - Editar Caixa D'Água")
-    println("3 - listar Caixa D'Água")
-    println("4 - Excluir Caixa D'Água")
-
-    val opcao = readln().toInt()
-
-    when(opcao) {
-        1 -> println("Cadastrando caixa...")
-        2 -> println("Editando caixa...")
-        3 -> println("listar caixa...")
-        4 -> println("Excluido caixa...")
-        else -> println("Opção inválida")
+    do {
 
 
-    }
+        println("1 - Cadastrar Caixa D'Água")
+        println("2 - Editar Caixa D'Água")
+        println("3 - listar Caixa D'Água")
+        println("4 - Excluir Caixa D'Água")
 
+        val opcao = readln().toInt()
+
+        when (opcao) {
+            1 -> println("Cadastrando caixa...")
+            2 -> println("Editando caixa...")
+            3 -> println("listar caixa...")
+            4 -> println("Excluido caixa...")
+            else -> println("Opção inválida")
+        }
+    } while (opcao != 0)
 }
-
